b749403311fab9683898350a2dc1dc597da755b8
Timothy Ng <ncy.timothy@gmail.com>
2017-11-24 08:40:35 -0800

    Add shared model using AppDelegate

---
+++ MemeMe/MemeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Timothy Ng on 11/24/17.
+//  Copyright © 2017 Timothy Ng. All rights reserved.
+//
+
+import Foundation
